eee3c31b3c89d6063c106d01ddf009da8c90867f
PedroYanezE <pedro.yanez.e@gmail.com>
2023-07-13 00:05:33 -0400

    feat: Implement method to read inventory and instantiate products with it

--- Inventory/Program.cs
+++ Inventory/Program.cs
@@ -1,22 +1,29 @@
-﻿public class Inventory
+﻿using System.Text;
+
+class Inventory
 {
-    private static Product[] products = Array.Empty<Product>();
+    private static List<Product> _products = new();
+    public List<Product> products
+    {
+        get { return _products; }
+    }
 
     public Inventory()
     {
-        // products = ReadFile();
+        // Populate products list
+        ReadInventoryFile();
     }
 
-    class Product
+    public class Product
     {
         public int _id;
-        private string? _name;
-        private int _price;
-        private int _quantity;
+        public string? _name;
+        public int _price;
+        public int _quantity;
 
-        public Product(string? name, int price, int quantity)
+        public Product(int id, string? name, int price, int quantity)
         {
-            _id = GenerateId();
+            _id = id;
             _name = name;
             _price = price;
             _quantity = quantity;
@@ -26,20 +33,41 @@
     protected static int GenerateId()
     {
         int maxId = 0;
-        for (int i = 0; i < products.Length; i++)
+        for (int i = 0; i < _products.Count; i++)
         {
-            if (products[i]._id > maxId)
+            if (_products[i]._id > maxId)
             {
-                maxId = products[i]._id;
+                maxId = _products[i]._id;
             }
         }
 
         return maxId;
     }
 
-    // TODO: Reads the "inventory.txt", then updates de products array accordingly.
-    private static Product[] ReadFile()
+    public static void ReadInventoryFile()
     {
-        return Array.Empty<Product>();
+        Console.WriteLine("READING INVENTORY");
+        foreach (string line in File.ReadLines(@"C:\Users\pedro\source\repos\Inventory\Inventory\inventory.txt"))
+        {
+            string[] splittedLine = line.Split(' ');
+
+            Product currentProduct = new Product(Int32.Parse(splittedLine[0]), splittedLine[1], Int32.Parse(splittedLine[2]), Int32.Parse(splittedLine[3]));
+            _products.Add(currentProduct);
+        }
+        Console.WriteLine("FINISHED READING INVENTORY");
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Check that products is being initialized correctly
+        Inventory inv = new Inventory();
+        for(int i = 0; i < inv.products.Count(); i++)
+        {
+            Console.WriteLine(inv.products[i]._id.ToString() + " " + inv.products[i]._name + " " + inv.products[i]._price + " " + inv.products[i]._quantity);
+        }
     }
 }
